6ab2c6dc1237e2cbf1abf0becc381eff7c82967e
Filipp Shamshin <filippsh@Filipps-MacBook-Pro.local>
2025-03-25 23:17:57 +0100

    Add Next.js configuration and environment variables example

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['openweathermap.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'openweathermap.org',
+        pathname: '/img/wn/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig; 
